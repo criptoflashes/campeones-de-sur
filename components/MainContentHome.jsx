8a358471c3b54ddade0b90a4ad81ad7196d5ab81
FloatingRiver8 <maceiramariana@gmail.com>
2023-07-16 17:33:07 -0300

    feat (navigation)  press/tap cheese animation

--- components/MainContentHome.jsx
+++ components/MainContentHome.jsx
@@ -12,7 +12,7 @@ function MainContentHome() {
             offset: ["start end",  "end end"]})
 
        
-        const smoothOpacity = useSpring(scrollYProgress , { stiffness: 100, damping: 50 })
+        const smoothOpacity = useSpring(scrollYProgress , { stiffness: 100, damping: 50, mass:0.5 })
 
     return (
         <div className="py-20 "> <div className="divider"></div>
@@ -21,13 +21,13 @@ function MainContentHome() {
 
 
                 <motion.div style={{ opacity: smoothOpacity   , scale: smoothOpacity  }}/* style={{ scaleX: scrollYProgress }} */  /* animate={{ opacity:scrollYProgress, scale: 1 }} */ /* transition={{ ease: "easeInOut", duration: 0.8 }}  */class="h-32 rounded-lg bg-gray-400"></motion.div>
-                <motion.div style={{ opacity: smoothOpacity    , scale: smoothOpacity   }}/* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ease: "easeInOut", duration: 2 }} */ class="h-32 rounded-lg bg-gray-400"></motion.div>
+                <motion.div  /* initial={{ opacity: 0.5, scale: 0.9 }} */ style={{ opacity: smoothOpacity    , scale: smoothOpacity }} class="h-32 rounded-lg bg-gray-400"></motion.div>
                 <motion.div style={{  opacity: smoothOpacity    , scale: smoothOpacity    }}/* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ease: "easeInOut", duration: 3 }} */ class="h-32 rounded-lg bg-gray-400 lg:col-span-2"></motion.div>
 
 
-                <motion.div style={{  opacity: smoothOpacity    , scale: smoothOpacity    }} /* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ease: "easeInOut", duration: 4 }} */ class="h-32 rounded-lg bg-gray-400 lg:col-span-2"></motion.div>
-                <motion.div style={{  opacity: smoothOpacity    , scale: smoothOpacity     }} /* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ease: "easeInOut", duration: 5 }} */ class="h-32 rounded-lg bg-gray-400"></motion.div>
-                <motion.div style={{  opacity: smoothOpacity    , scale: smoothOpacity    }} /* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ ease: "easeInOut",duration: 6 }} */class="h-32 rounded-lg bg-gray-400"></motion.div>
+                <motion.div style={{ opacity: smoothOpacity , scale: smoothOpacity }} /* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ease: "easeInOut", duration: 4 }} */ class="h-32 rounded-lg bg-gray-400 lg:col-span-2"></motion.div>
+                <motion.div style={{ opacity: smoothOpacity    , scale: smoothOpacity  }} /* initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ease: "easeInOut", duration: 5 }} */ class="h-32 rounded-lg bg-gray-400"></motion.div>
+                <motion.div style={{ opacity: smoothOpacity    , scale: smoothOpacity }} class="h-32 rounded-lg bg-gray-400"></motion.div>
 
 
             </div>
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,14 +1,15 @@
+'use client'
 
 import Link from 'next/link'
 import Image from 'next/image'
 import cheeseHome from '@/public/cheese-svgrepo-com.svg'
-
+import { motion } from "framer-motion"
 
 
 function Navigation() {
     return (
         <div /* className='relative' */>
-           
+
             <div className="navbar  bg-blue-300">
                 <div className="navbar-start  ">
                     <div className="dropdown">
@@ -34,39 +35,41 @@ function Navigation() {
                             </li>
                         </ul>
                     </div>
-                    <div className='hover:scale-75 translate-x-20 skew-y-3 '>
+                    <motion.div whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17 }}>
                     <Link href="/">
-                    <Image src= {cheeseHome} alt="campeones del sur"  width="100" height="100" className='ml-20' />
+                        <Image src={cheeseHome} alt="campeones del sur" width="100" height="100" className='ml-20' />
                     </Link>
-                    </div>
-                </div>
-
-          
-                <div className="navbar-center hidden lg:flex  ">
-
-               
-                    <ul className="menu menu-horizontal px-1">
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Productos</summary>
-                                <ul className="p-2">
-                                    <li><Link href="/productos/quesos">Quesos</Link></li>
-                                    <li><Link href="/productos/chorizos">Chorizos</Link></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li>
-                            <Link href="/quienessomos">Quienes somos</Link>
-                        </li>
-                    </ul>
-                </div>
-
+                </motion.div>
             </div>
-            
+
+
+            <div className="navbar-center hidden lg:flex  ">
+
+
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li tabIndex={0}>
+                        <details>
+                            <summary>Productos</summary>
+                            <ul className="p-2">
+                                <li><Link href="/productos/quesos">Quesos</Link></li>
+                                <li><Link href="/productos/chorizos">Chorizos</Link></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li>
+                        <Link href="/quienessomos">Quienes somos</Link>
+                    </li>
+                </ul>
+            </div>
+
         </div>
+            
+        </div >
     )
 }
 
