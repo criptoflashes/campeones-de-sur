c3b96ddd60c8ed3c978fdd980c3e59a55fa5a8c4
FloatingRiver8 <maceiramariana@gmail.com>
2023-08-16 20:26:09 -0300

    fix some @ routes to simple paths

--- app/api/ping/route.js
+++ app/api/ping/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import  {connectDb } from '@/utils/mongooseConn'
+import  {connectDb } from '../../../utils/mongooseConn'
 
  export function GET(){
     connectDb()
--- app/api/quesos/[id]/route.js
+++ app/api/quesos/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectDb } from "@/utils/mongooseConn";
-import Product from "@/models/products";
+import Product from "../../../../models/products";
 
 export async function GET(request, { params }) {
   console.log(params);
