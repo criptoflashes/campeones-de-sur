449c12f667fe5f964cbd9bd10ee27114438c3511
FloatingRiver8 <maceiramariana@gmail.com>
2023-08-21 10:57:14 -0300

    fix CardAdminProducts tag

--- app/adminDashboard/page.jsx
+++ app/adminDashboard/page.jsx
@@ -5,9 +5,6 @@ import { useRouter, useParams } from "next/navigation"
 
 
 
-
-
-
 function AdminDashboard() {
 
   const router = useRouter()
@@ -60,66 +57,66 @@ function AdminDashboard() {
     }
   }
 
-    //Submit
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      createProduct()
-    }
-
-    const handleChange = (e) => {
-      console.log(e.target.value)
-      console.log(e.target.name)
-      setNewProduct({ ...newProduct, [e.target.name]: e.target.value })
-
-    }
-
-
-    useEffect(() => {
-      console.log(params)
-    }, [])
-
-
-    return (
-      <div className="h-screen flex justify-center items-center">
-
-        <form onSubmit={handleSubmit} >
-          <header className="flex justify-between">
-
-            <h1 className="font-bold text-3xl">{
-              !params.id ? "Crea un producto" : "Edita un producto"
-            }</h1>
-            <button type="button" className="bg-red-500 px-3 pt-1 rounded-md" onClick={handleDelete}>
-              Borrar producto
-            </button>
-
-
-          </header>
-          <input type="text" name="title" placeholder="Título" className="bg-gray-300 border-2 w-full p-4 rounded-lg my-4" onChange={handleChange} />
-
-
-          <select name="category" className="bg-gray-300 border-2 w-1/2 p-4 rounded-lg my-4" onChange={handleChange}>
-            <optgroup>
-              <option disabled selected>Selecciona una categoría</option>
-
-
-              <option value="Queso">Queso</option>
-              <option value="Chorizo">Chorizo</option>
-              <option value="Yerba">Yerba</option>
-              <option value="Manteca">Manteca</option>
-              <option value="Ajo">Ajo</option>
-              <option value="Miel">Miel</option>
-              <option value="Otros">Otros</option>
-            </optgroup>
-          </select>
-
-
-          <textarea name="description" placeholder="Descripción" className="bg-gray-300 border-2 w-full p-4 rounded-lg my-4" rows={3} onChange={handleChange} > </textarea>
-          <button type="submit" className="bg-green-600 hover:bg-green-700 text-white font-bold px-4  py-2 rounded-lg">Guardar</button>
-
-        </form>
-
-      </div>
-    )
+  //Submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createProduct()
   }
 
-  export default AdminDashboard
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    console.log(e.target.name)
+    setNewProduct({ ...newProduct, [e.target.name]: e.target.value })
+
+  }
+
+
+  useEffect(() => {
+    console.log(params)
+  }, [])
+
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+
+      <form onSubmit={handleSubmit} >
+        <header className="flex justify-between">
+
+          <h1 className="font-bold text-3xl">{
+            !params.id ? "Crea un producto" : "Edita un producto"
+          }</h1>
+          <button type="button" className="bg-red-500 px-3 pt-1 rounded-md" onClick={handleDelete}>
+            Borrar producto
+          </button>
+
+
+        </header>
+        <input type="text" name="title" placeholder="Título" className="bg-gray-300 border-2 w-full p-4 rounded-lg my-4" onChange={handleChange} />
+
+
+        <select name="category" className="bg-gray-300 border-2 w-1/2 p-4 rounded-lg my-4" onChange={handleChange}>
+          <optgroup>
+            <option disabled selected>Selecciona una categoría</option>
+
+
+            <option value="Queso">Queso</option>
+            <option value="Chorizo">Chorizo</option>
+            <option value="Yerba">Yerba</option>
+            <option value="Manteca">Manteca</option>
+            <option value="Ajo">Ajo</option>
+            <option value="Miel">Miel</option>
+            <option value="Otros">Otros</option>
+          </optgroup>
+        </select>
+
+
+        <textarea name="description" placeholder="Descripción" className="bg-gray-300 border-2 w-full p-4 rounded-lg my-4" rows={3} onChange={handleChange} > </textarea>
+        <button type="submit" className="bg-green-600 hover:bg-green-700 text-white font-bold px-4  py-2 rounded-lg">Guardar</button>
+
+      </form>
+
+    </div>
+  )
+}
+
+export default AdminDashboard
--- app/adminProducts/page.jsx
+++ app/adminProducts/page.jsx
@@ -15,15 +15,13 @@ async function loadProducts() {
 
 
 
-
-
 async function DashboardCardContainer() {
   const showProducts = await loadProducts()
   return (
     <div className= "grid md:grid-cols-3 gap-2 ">
       {/* {JSON.stringify(showProducts)} */}
         {showProducts.map(product => (
-        <CardAdminProducts product={product} key={product._id}></CardAdminProducts>
+        <CardAdminProducts product={product} key={product._id}/>
       ))}  
     </div>
   )
