82189ab0e41b7f25d852d7447398f30a4ba651d4
FloatingRiver8 <maceiramariana@gmail.com>
2023-08-21 11:16:53 -0300

    next config modified taking away experimental

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-      experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] },
-    webpack(config) {
-        config.experiments = { ...config.experiments, topLevelAwait: true };
-        return config;
-},
 images : {
   domains : ['images.pexels.com']
 }}
