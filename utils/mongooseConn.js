d5a851aeec07b5345a8ef2025501082aac96e71f
FloatingRiver8 <maceiramariana@gmail.com>
2023-09-11 16:35:22 -0300

    fix mongo connection adding variable for process.env

--- utils/mongooseConn.js
+++ utils/mongooseConn.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 import {connection, connect }from 'mongoose'
-
+const source = process.env.MONGODB_URI
 const conn = {
     isConnected: false
 }
@@ -10,11 +10,11 @@ export async function connectDb(){
 //if already connected don't repeat the connection again if not, continue code and connect
 if(conn.isConnected) return
 
-   const db = await connect(process.env.MONGODB_URI)/* mongodb://localhost/campeones */
+   const db = await connect(source)/* mongodb://localhost/campeones */
 console.log(db.connection.db.databaseName)
 conn.isConnected = db.connections[0].readyState
 }
-
+console.log("uri", source)
 
 connection.on('connected', () => {
     console.log("Mongoose is connected")
