93387cb2f8d96357cd2cbf3f06e4e9e3a3cf69c7
FloatingRiver8 <maceiramariana@gmail.com>
2023-10-10 20:09:11 -0300

    fix cors allow origin

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ async headers() {
       source: "/api/:path*",
       headers: [
         { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "https://vercel.com/criptoflashes/campeones-de-sur/adminDashboard" },
+        { key: "Access-Control-Allow-Origin", value: "https://campeones-del-sur.vercel.app/adminDashboard" },
         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
       ]
