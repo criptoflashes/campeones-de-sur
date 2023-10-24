5ad996962daf8786eac2957c4b19b89adcb7564c
FloatingRiver8 <maceiramariana@gmail.com>
2023-10-24 20:14:55 -0300

    add styles, text size and colors in allProduct cards

--- components/CardProducts.jsx
+++ components/CardProducts.jsx
@@ -9,18 +9,24 @@ function CardProducts({ product }) {
         > */
     <div
       className="bg-gray-400 text-white rounded-md hover:cursor-pointer hover:bg-gray-700 my-1">
+
+      <div
+        class="border-b-4 border-neutral-100 px-6 py-3 my-6 dark:border-neutral-300 dark:text-neutral-50">
+        <h1 className="text-lg font-bold ">{product.title}</h1>
+      </div>
+
       <div className="relative ">
         {product.imageUrl && (
           <img
             src={product.imageUrl}
-            className="w-full h-48 object-cover rounded-t-lg"
+            className="w-full h-48 object-cover "
             alt=""
           />
         )}
         <div className="p-4">
-          <h1 className="text-lg font-bold">{product.title}</h1>
-          <h2 className="text-2xl">{product.category}</h2>
-          <h2 className="text-sm text-slate-600">{product.description}</h2>
+          {/* <h1 className="text-lg font-bold">{product.title}</h1> */}
+          {/* <h2 className="text-2xl">{product.category}</h2> */}
+          <h2 className="text-lg text-slate-100">{product.description}</h2>
         </div>
       </div>
 
