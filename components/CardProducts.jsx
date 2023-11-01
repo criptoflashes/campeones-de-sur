f0c53af46a79c329912dc4f4b7f19326e858eaa3
FloatingRiver8 <maceiramariana@gmail.com>
2023-11-01 20:13:48 -0300

    fix link tag to encapsule the hole card

--- components/CardProducts.jsx
+++ components/CardProducts.jsx
@@ -6,28 +6,28 @@ function CardProducts({ product }) {
 
     <div
       className="bg-gray-400 text-white rounded-md hover:cursor-pointer hover:bg-gray-700 my-1">
+      <Link href={`/cardProductsDetail/${product._id}`}>
+        <div
+          class="border-b-4 border-neutral-100 px-6 py-3 my-6 dark:border-neutral-300 dark:text-neutral-50">
+          <h1 className="text-lg font-bold ">{product.title}</h1>
+        </div>
 
-      <div
-        class="border-b-4 border-neutral-100 px-6 py-3 my-6 dark:border-neutral-300 dark:text-neutral-50">
-        <h1 className="text-lg font-bold ">{product.title}</h1>
-      </div>
+        <div className="relative ">
+          {product.imageUrl && (
+            <img
+              src={product.imageUrl}
+              className="w-full h-48 object-cover "
+              alt=""
+            />
+          )}
 
-      <div className="relative ">
-        {product.imageUrl && (
-          <img
-            src={product.imageUrl}
-            className="w-full h-48 object-cover "
-            alt=""
-          />
-        )}
-
-      </div>
+        </div>
 
 
-      <div>
-        <Link href={`/cardProductsDetail/${product._id}`}><h2>+ info</h2> </Link>
-      </div>
-
+        <div>
+          <h2>+ info</h2>
+        </div>
+      </Link>
     </div>
   );
 }
