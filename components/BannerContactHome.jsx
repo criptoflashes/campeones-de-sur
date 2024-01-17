bf6a8e34f622c86aec86e98b602026ebd73f846d
FloatingRiver8 <maceiramariana@gmail.com>
2024-01-17 18:50:16 -0300

    add new component ArrowWhats

--- components/BannerContactHome.jsx
+++ components/BannerContactHome.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { motion } from "framer-motion"
+import WhatsArrows from "./whatsArrows"
 
 
 function BannerContactHome() {
@@ -13,25 +14,7 @@ function BannerContactHome() {
                 <div className="pt-8  ">
                     <h1 className=" text-4xl text-slate-600 text-center	">Contactanos por Whatsapp para hacer tu pedido y saber más!</h1>
                     <div className='flex justify-center  '>
-                        <div className="px-4 py-3 text-white" >
-                        <motion.div animate={{
-                            scale: [1],
-                             x: [0,50]  ,
-                            opacity: [1,0.5,0]
-                        }}
-                            transition={{
-                                duration: 1,
-                                ease: "easeInOut",
-                                times: [ 1 ],
-                                repeat: "infinite",
-                                repeatDelay: 1
-
-                            }} className="flex flex-row place-content-center w-200 h-200">
-                            
-                            <svg xmlns="http://www.w3.org/2000/svg" width="74" height="74" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M13 17l5-5-5-5M6 17l5-5-5-5"/></svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="74" height="74" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M13 17l5-5-5-5M6 17l5-5-5-5"/></svg>
-                        </motion.div>
-                        </div>
+                       <WhatsArrows/>
                     </div>
                 </div>
             </div>
